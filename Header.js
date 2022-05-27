49ac87941a5093fdd660f042426d48013300f228
Akshay Gale <90633510+Akshaygale@users.noreply.github.com>
2022-05-27 17:07:41 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styleme.css'
+
+const Header = () => {
+  return (
+    <div>
+    
+       <nav>
+       <div>
+    <h2>Bella</h2>
+       </div>
+       <div className='flex-lists'>
+         <div>Home</div>
+         <div>Shop</div>
+         <div>Favourites</div>
+         <div>Orders</div>
+         <div>Contacts</div>
+         </div>
+          <div className='carts-lists'>
+                <h5>Profile</h5>
+                <div><i className="fa-solid fa-cart-shopping fa-3x"></i></div>
+            </div>
+        </nav>
+    <div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default Header
